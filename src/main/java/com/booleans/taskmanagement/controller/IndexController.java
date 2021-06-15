831ece849f4b9f03b9f7cbd5390b9cd48e7456e0
YasaswiS <yasaswisingamneni2004@gmail.com>
2021-06-14 19:06:21 -0700

    added API function to list all users

--- src/main/java/com/booleans/taskmanagement/controller/IndexController.java
+++ src/main/java/com/booleans/taskmanagement/controller/IndexController.java
@@ -1,13 +1,36 @@
 package com.booleans.taskmanagement.controller;
 
+import com.booleans.taskmanagement.model.Task;
+import com.booleans.taskmanagement.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.booleans.taskmanagement.service.UserService;
+import com.booleans.taskmanagement.model.User;
+
+import java.util.List;
 
 @Controller
 public class IndexController {
 
+    private UserService userService;
+
+    @Autowired
+    public IndexController(UserService userService) {
+        this.userService = userService;
+    }
+
+
     @RequestMapping("/")
     String showIndex() {
         return "index";
+    }
+
+    @RequestMapping("/allusers")
+    public ResponseEntity<List<User>> users(){
+        return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
+
     }
 }
